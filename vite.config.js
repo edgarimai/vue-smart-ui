c246d8bb377bfeb6d00dfb19034718983847ff5a
Edgar Imai <edgar.imai@outlook.com>
2025-02-26 00:26:59 -0300

    chore: update git repository url, configure Vite for library build and update package exports

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve, dirname } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.js'),
+      name: 'VueSmartUI',
+      fileName: (format) => `vue-smart-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
